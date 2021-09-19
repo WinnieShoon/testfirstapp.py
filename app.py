a44060fc75c96162f568059e383f2fce64d97305
WinnieShoon <90586982+WinnieShoon@users.noreply.github.com>
2021-09-19 22:18:37 +0630

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ import pandas as pd
 
 st.write("""
 # Simple Stock Price App
+
 Shown are the stock **Closing Price** and **Volume** of Google!
+
 """)
 
 # https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
